5289e06e95a76a2d216c3a264a1ff46c1903c769
sanjeev <sanjeevsingh.77d@gmail.com>
2022-10-14 08:04:51 +0530

    Solving using Case Statement

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace EmployeeWage {
     internal class Program {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args) {
             // Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+           
             // Variables
             int empHrs = 0;
             int empWage = 0;
@@ -12,14 +13,17 @@
 
             // Computation
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME) {
-                empHrs = 4;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else if (empCheck == IS_FULL_TIME) {
-                empHrs = 8;
-            } else { 
-                empHrs = 0;
-              }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
         }
