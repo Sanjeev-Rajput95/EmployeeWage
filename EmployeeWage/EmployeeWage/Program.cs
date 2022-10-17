976d6c3ef39a0f5931a18c3c4200c338c583d880
sanjeev <sanjeevsingh.77d@gmail.com>
2022-10-17 18:36:39 +0530

    Ability to save the Total Wage for Each Company

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -9,8 +9,8 @@ namespace EmployeeWage
 
         static void Main(string[] args)
         {
-            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMART", 20, 2, 10);
-            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Relience", 10, 4, 20);
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMART ", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Relience ", 10, 4, 20);
             dMart.computeEmpWage();
             Console.WriteLine(dMart.toString());
             reliance.computeEmpWage();
